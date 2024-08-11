0f27df3a9e533464d8b80df007fbac290ce4e9dd
thayu543 <149858189+thayu543@users.noreply.github.com>
2024-08-11 21:02:13 +0530

    Add files via upload

---
+++ Register.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Employee_Management_Application_using_C_.NET
+{
+    partial class Registration_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration_Form));
+            panel1 = new Panel();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            linkLabel2 = new LinkLabel();
+            label4 = new Label();
+            button1 = new Button();
+            spassword_tb = new TextBox();
+            username_tb = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            label6 = new Label();
+            email_tb = new TextBox();
+            label7 = new Label();
+            mobilenum_tb = new TextBox();
+            pictureBox2 = new PictureBox();
+            linkLabel1 = new LinkLabel();
+            cpassword_tb = new TextBox();
+            label8 = new Label();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Blue;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(289, 785);
+            panel1.TabIndex = 21;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Blue;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(22, 363);
+            label5.Name = "label5";
+            label5.Size = new Size(243, 23);
+            label5.TabIndex = 11;
+            label5.Text = "Employee management App";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(47, 121);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(190, 215);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(783, 734);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(54, 20);
+            linkLabel2.TabIndex = 19;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Sign In";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(714, 682);
+            label4.Name = "label4";
+            label4.Size = new Size(225, 23);
+            label4.TabIndex = 18;
+            label4.Text = "Already Have an Account";
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(0, 192, 192);
+            button1.Location = new Point(363, 659);
+            button1.Name = "button1";
+            button1.Size = new Size(214, 46);
+            button1.TabIndex = 16;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // spassword_tb
+            // 
+            spassword_tb.Location = new Point(363, 376);
+            spassword_tb.Name = "spassword_tb";
+            spassword_tb.Size = new Size(396, 27);
+            spassword_tb.TabIndex = 15;
+            // 
+            // username_tb
+            // 
+            username_tb.Location = new Point(355, 124);
+            username_tb.Name = "username_tb";
+            username_tb.Size = new Size(396, 27);
+            username_tb.TabIndex = 14;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(363, 321);
+            label3.Name = "label3";
+            label3.Size = new Size(250, 35);
+            label3.TabIndex = 13;
+            label3.Text = "Confirm password";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(353, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(147, 35);
+            label2.TabIndex = 12;
+            label2.Text = "UserName";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(572, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(265, 51);
+            label1.TabIndex = 11;
+            label1.Text = "Registration ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(363, 447);
+            label6.Name = "label6";
+            label6.Size = new Size(89, 35);
+            label6.TabIndex = 22;
+            label6.Text = "Email";
+            // 
+            // email_tb
+            // 
+            email_tb.Location = new Point(363, 496);
+            email_tb.Name = "email_tb";
+            email_tb.Size = new Size(396, 27);
+            email_tb.TabIndex = 23;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(363, 561);
+            label7.Name = "label7";
+            label7.Size = new Size(174, 35);
+            label7.TabIndex = 24;
+            label7.Text = "Mobile Num";
+            // 
+            // mobilenum_tb
+            // 
+            mobilenum_tb.Location = new Point(363, 610);
+            mobilenum_tb.Name = "mobilenum_tb";
+            mobilenum_tb.Size = new Size(396, 27);
+            mobilenum_tb.TabIndex = 25;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1121, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(41, 37);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 26;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(443, 734);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(45, 20);
+            linkLabel1.TabIndex = 27;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Reset";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // cpassword_tb
+            // 
+            cpassword_tb.Location = new Point(357, 253);
+            cpassword_tb.Name = "cpassword_tb";
+            cpassword_tb.Size = new Size(396, 27);
+            cpassword_tb.TabIndex = 29;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(355, 202);
+            label8.Name = "label8";
+            label8.Size = new Size(137, 35);
+            label8.TabIndex = 28;
+            label8.Text = "Password";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(1083, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(41, 37);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 30;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(1046, 0);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(41, 37);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 31;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // Registration_Form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1161, 785);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(cpassword_tb);
+            Controls.Add(label8);
+            Controls.Add(linkLabel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(mobilenum_tb);
+            Controls.Add(label7);
+            Controls.Add(email_tb);
+            Controls.Add(label6);
+            Controls.Add(panel1);
+            Controls.Add(linkLabel2);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(spassword_tb);
+            Controls.Add(username_tb);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Registration_Form";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Register";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private LinkLabel linkLabel2;
+        private Label label4;
+        private Button button1;
+        private TextBox spassword_tb;
+        private TextBox username_tb;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label6;
+        private TextBox email_tb;
+        private Label label7;
+        private TextBox mobilenum_tb;
+        private PictureBox pictureBox2;
+        private LinkLabel linkLabel1;
+        private TextBox cpassword_tb;
+        private Label label8;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+    }
+}
