350501e3bfa0d7192fa0e54a28b043fa47fc09c5
thayu543 <149858189+thayu543@users.noreply.github.com>
2024-08-11 21:34:23 +0530

    Add files via upload

---
+++ ForgetPassword.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Employee_Management_Application_using_C_.NET
+{
+    partial class ForgetPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForgetPassword));
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(459, 540);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(396, 27);
+            textBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(459, 218);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(396, 27);
+            textBox1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(459, 485);
+            label3.Name = "label3";
+            label3.Size = new Size(155, 35);
+            label3.TabIndex = 18;
+            label3.Text = "Enter OTP";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(459, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(293, 35);
+            label2.TabIndex = 17;
+            label2.Text = "Enter Registered Mail";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(471, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(362, 51);
+            label1.TabIndex = 16;
+            label1.Text = "Email Verification";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(0, 0, 192);
+            button1.Location = new Point(717, 596);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 56);
+            button1.TabIndex = 21;
+            button1.Text = "VERIFY OTP";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(0, 0, 192);
+            button2.Location = new Point(710, 285);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 56);
+            button2.TabIndex = 22;
+            button2.Text = "SEND OTP";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Blue;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label4);
+            panel1.Dock = DockStyle.Left;
+            panel1.ForeColor = Color.White;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(274, 747);
+            panel1.TabIndex = 23;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(36, 143);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(190, 215);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 25;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 418);
+            label4.Name = "label4";
+            label4.Size = new Size(246, 23);
+            label4.TabIndex = 24;
+            label4.Text = "Employee Management App";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1092, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(68, 57);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 24;
+            pictureBox2.TabStop = false;
+            // 
+            // ForgetPassword
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1156, 747);
+            Controls.Add(pictureBox2);
+            Controls.Add(panel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ForgetPassword";
+            Text = "ForgetPassword";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
