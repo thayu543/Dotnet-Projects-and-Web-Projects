ee33be2622208cf96017f53da69da912aa1d9b7a
thayu543 <149858189+thayu543@users.noreply.github.com>
2024-08-11 21:38:22 +0530

    Add files via upload

---
+++ EmpDashboard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee_Management_Application_using_C_.NET
+{
+    public partial class EmpDashboard : Form
+    {
+        public EmpDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void EmpDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            groupBox2.Visible = false;
+        }
+    }
+}
