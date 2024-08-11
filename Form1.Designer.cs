7c4154a2fd016eae0c48e71e0444b1a6d405ab53
thayu543 <149858189+thayu543@users.noreply.github.com>
2024-08-11 20:59:18 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Employee_Management_Application_using_C_.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            loguname_tb = new TextBox();
+            lgpass_tb = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            label4 = new Label();
+            linkLabel2 = new LinkLabel();
+            checkBox1 = new CheckBox();
+            panel1 = new Panel();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            linkLabel3 = new LinkLabel();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(441, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 51);
+            label1.TabIndex = 0;
+            label1.Text = "Login Page";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(393, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(147, 35);
+            label2.TabIndex = 1;
+            label2.Text = "UserName";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(393, 247);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 35);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // loguname_tb
+            // 
+            loguname_tb.Location = new Point(393, 182);
+            loguname_tb.Name = "loguname_tb";
+            loguname_tb.Size = new Size(396, 27);
+            loguname_tb.TabIndex = 3;
+            // 
+            // lgpass_tb
+            // 
+            lgpass_tb.Location = new Point(393, 292);
+            lgpass_tb.Name = "lgpass_tb";
+            lgpass_tb.PasswordChar = '*';
+            lgpass_tb.Size = new Size(396, 27);
+            lgpass_tb.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(0, 192, 192);
+            button1.Location = new Point(469, 375);
+            button1.Name = "button1";
+            button1.Size = new Size(214, 46);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(393, 335);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(117, 20);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forget Password";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(842, 415);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 23);
+            label4.TabIndex = 7;
+            label4.Text = "Create an Account";
+            label4.Click += label4_Click;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(882, 454);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(61, 20);
+            linkLabel2.TabIndex = 8;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Sign Up";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(657, 345);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(132, 24);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "Show Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Blue;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(289, 609);
+            panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Blue;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(22, 363);
+            label5.Name = "label5";
+            label5.Size = new Size(243, 23);
+            label5.TabIndex = 11;
+            label5.Text = "Employee management App";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(47, 121);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(190, 215);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Location = new Point(564, 454);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(45, 20);
+            linkLabel3.TabIndex = 28;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Reset";
+            linkLabel3.LinkClicked += linkLabel3_LinkClicked;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(1031, 0);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(41, 37);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 34;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(1068, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(41, 37);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 33;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1106, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(41, 37);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 32;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1146, 609);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(linkLabel3);
+            Controls.Add(panel1);
+            Controls.Add(checkBox1);
+            Controls.Add(linkLabel2);
+            Controls.Add(label4);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(lgpass_tb);
+            Controls.Add(loguname_tb);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox loguname_tb;
+        private TextBox lgpass_tb;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private Label label4;
+        private LinkLabel linkLabel2;
+        private CheckBox checkBox1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label5;
+        private LinkLabel linkLabel3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+    }
+}
