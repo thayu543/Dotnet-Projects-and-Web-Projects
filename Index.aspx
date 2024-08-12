787c01bf69f46b4fb6276d170bb6099ddd728d31
thayu543 <149858189+thayu543@users.noreply.github.com>
2024-08-12 15:44:42 +0530

    Add files via upload

---
+++ Index.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ProductWebSite.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+ </div>
+               product code:
+<asp:textbox id="txt_product_code" runat="server"></asp:textbox><br/><br/>
+        product name:
+<asp:textbox id="txt_product_name" runat="server"></asp:textbox><br/><br/>
+          quantity:
+<asp:textbox id="txt_quantity" runat="server"></asp:textbox><br/><br/>
+        rate:
+<asp:textbox id="txt_rate" runat="server"></asp:textbox><br/><br/>
+        percentage:
+<asp:textbox id="txt_percentage" runat="server"></asp:textbox><br/><br/>
+  
+<asp:button id="btninsert" runat="server" text="insert" onclick="insert"/>
+<hr/>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace ProductWebSite
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class saneproducts
+        {
+
+
+            public int? product_code { get; set; }
+
+            [StringLength(50)]
+            public string product_name { get; set; }
+            public int? quantity { get; set; }
+
+            public double? rate { get; set; }
+
+            public double? taxamount { get; set; }
+
+            public double? taxpercentage { get; set; }
+
+            public double? grosstotal { get; set; }
+        }
+
+        protected void insert(object sender, EventArgs e)
+        {
+            string ApiUrl = "https://localhost:44318/api/tbl_product_master_trans";
+
+            var thayuproduct = new
+            {
+                product_code = txt_product_code.Text.Trim(),
+                product_name = txt_product_name.Text.Trim(),
+                quantity = txt_quantity.Text.Trim(),
+                rate = txt_rate.Text.Trim(),
+                taxpercentage = txt_percentage.Text.Trim(),
+                taxamount = (
+                           Convert.ToInt32(txt_quantity.Text.Trim()) *
+                           Convert.ToInt32(txt_rate.Text.Trim())
+                           ) / Convert.ToInt32(txt_percentage.Text.Trim()),
+
+
+                grosstotal = (
+                           Convert.ToInt32(txt_quantity.Text.Trim()) *
+                           Convert.ToInt32(txt_rate.Text.Trim())
+                               ) +
+
+                               (
+                           Convert.ToInt32(txt_quantity.Text.Trim()) *
+                           Convert.ToInt32(txt_rate.Text.Trim())
+                           ) / Convert.ToInt32(txt_percentage.Text.Trim()),
+
+            };
+            string inputjson = (new JavaScriptSerializer()).Serialize(thayuproduct);
+            HttpClient client = new HttpClient();
+            HttpContent inputcontent = new StringContent(inputjson, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(ApiUrl + "/tbl_product_master_trans", inputcontent).Result;
+        }
+    }
+}
