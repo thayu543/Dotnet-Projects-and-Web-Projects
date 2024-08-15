e3b62acff20bfb82c0c0dae4abb968715eeefc58
thayu543 <149858189+thayu543@users.noreply.github.com>
2024-08-15 10:52:01 +0530

    Add files via upload

---
+++ MVC Projects/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project_Using_Entity_framework
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
